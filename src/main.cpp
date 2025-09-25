449d96c0ccf96904aac109ec2834c0ccbb4cb4ae
Aaron S Grincewicz <AaronSG@Outlook.com>
2025-09-24 21:50:55 -0400

    redo

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,4 @@
 //  // Unauthorized use, modification, or redistribution is prohibited.
 //  /
 
-#include <iostream>
-// Initialize system
-// Load user data
-// Display main menu
-// Handle user input
-// Route to appropriate module
-#include "UserManager.h"
 
-int main() {
-    UserManager um;
-    um.registerUser("Craig", "legacy123");
-    return 0;
-}
