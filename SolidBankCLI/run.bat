0ba672b7b1968d2a0c74d8d80ec41e0d936e4a8c
Aaron S Grincewicz <70443410+ASGrincewicz@users.noreply.github.com>
2025-09-30 16:10:15 -0400

    Delete SolidBankCLI directory

--- SolidBankCLI/run.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cmake --preset windows-debug
-cmake --build --preset windows-debug
-cmake-build-win\SolidBankCLI.exe
--- SolidBankCLI/src/transaction.cpp
+++
@@ -1,7 +0,0 @@
-//
-//  // Copyright © 2025 Aaron Steven Grincewicz
-//  // All rights reserved.
-//  // Unauthorized use, modification, or redistribution is prohibited.
-//  /
-
-#include "transaction.h"
