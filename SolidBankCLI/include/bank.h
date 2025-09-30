f95de47540ed2b8d3ae75afb2b01af0759551614
ASGrincewicz <AaronSG@outlook.com>
2025-09-30 15:50:03 -0400

    add cmake presets

---
+++ SolidBankCLI/include/bank.h
@@ -0,0 +1,27 @@
+//
+//  // Copyright © 2025 Aaron Steven Grincewicz
+//  // All rights reserved.
+//  // Unauthorized use, modification, or redistribution is prohibited.
+//  /
+
+#pragma once
+#include <vector>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+#include "transaction.h"
+#include "utils.h"
+
+class Bank {
+public:
+    Bank();
+    double getBalance() const;
+    bool deposit(double);
+    bool withdraw(double);
+    const std::vector<Transaction>& getTransactions() const;
+    std::string getFormattedLedger() const;
+
+private:
+    double balance;
+    std::vector<Transaction> transactions;
+};
